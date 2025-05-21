194b23fc93ef848c4f45a423f660e02c27078f85
Abhishek Yadav <monuy8830@gmail.com>
2025-05-21 10:29:11 +0530

    fix: comment out unused list items in StoreHome component

--- src/pages/StoreHome.jsx
+++ src/pages/StoreHome.jsx
@@ -291,7 +291,7 @@ const StoreHome = () => {
             </Link>
           </li>
 
-          <li>
+          {/* <li>
             <Link
               to={"/store/customization?storeTab=single-setting"}
               className={`inline-block w-full py-3 px-4 shadow-md ${
@@ -307,7 +307,7 @@ const StoreHome = () => {
                 {"Setting"}
               </span>
             </Link>
-          </li>
+          </li> */}
 
           <li>
             <Link
@@ -398,7 +398,7 @@ const StoreHome = () => {
               </span>
             </Link>
           </li>
-          <li>
+          {/* <li>
             <Link
               to={"/store/customization?storeTab=checkout-setting"}
               className={`inline-block w-full py-3 px-4 shadow-md ${
@@ -414,8 +414,8 @@ const StoreHome = () => {
                 {t("Checkout")}
               </span>
             </Link>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <Link
               to={"/store/customization?storeTab=dashboard-setting"}
               className={`inline-block w-full py-3 px-4 shadow-md ${
@@ -431,7 +431,7 @@ const StoreHome = () => {
                 {t("Dashboard")}
               </span>
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link
               to={"/store/customization?storeTab=seo-settings"}
